e5b0521b71c61d06ca83d9357856c60848a8cf17
Linebotdemo <k.inose0902@gmail.com>
2025-07-14 23:34:16 +0900

    ♻️ Move backend contents to root

--- app.py
+++ app.py
@@ -1240,4 +1240,4 @@ with app.app_context():
         db.session.commit()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
